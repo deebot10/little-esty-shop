b74818c4aadd80c6856410e36f773f81cf609e75
deebot10 <dee1jhill@gmail.com>
2021-08-08 17:33:11 -0600

    Pass Story4

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -25,6 +25,14 @@ class Merchant::BulkDiscountsController < ApplicationController
     end  
   end
 
+  def destroy
+    # require 'pry'; binding.pry
+    merchant = Merchant.find(params[:merchant_id])
+    discount = BulkDiscount.find(params[:id])
+    merchant.bulk_discounts.destroy(discount)
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
+
   private
 
   def discount_params
